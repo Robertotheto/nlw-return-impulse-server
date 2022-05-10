0d3aa3baa6e446ec180a922127336de987ef6b5c
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2022-05-10 00:35:43 -0400

    Add port variable.

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,6 @@ server.use(cors());
 server.use(express.json());
 server.use(routes)
 
-server.listen(3333, () => {
+server.listen(process.env.PORT || 3333, () => {
   console.log("Server is running on port 3333");
 })
